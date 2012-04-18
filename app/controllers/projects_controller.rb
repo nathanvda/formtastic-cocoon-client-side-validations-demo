7b9d8bf39cd04ec7112fccc140165fecc6877a96
nathanvda <nathan@dixis.com>
2012-04-18 08:12:28 +0200

    Use json instead of xml.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -17,7 +17,7 @@ class ProjectsController < ApplicationController
 
     respond_to do |format|
       format.html # show.html.erb
-      format.xml  { render :xml => @project }
+      format.json  { render :json => @project }
     end
   end
 
@@ -28,7 +28,7 @@ class ProjectsController < ApplicationController
 
     respond_to do |format|
       format.html # new.html.erb
-      format.xml  { render :xml => @project }
+      format.json  { render :json => @project }
     end
   end
 
@@ -45,10 +45,10 @@ class ProjectsController < ApplicationController
     respond_to do |format|
       if @project.save
         format.html { redirect_to(@project, :notice => 'Project was successfully created.') }
-        format.xml  { render :xml => @project, :status => :created, :location => @project }
+        format.json  { render :json => @project, :status => :created, :location => @project }
       else
         format.html { render :action => "new" }
-        format.xml  { render :xml => @project.errors, :status => :unprocessable_entity }
+        format.json  { render :xml => @project.errors, :status => :unprocessable_entity }
       end
     end
   end
@@ -61,10 +61,10 @@ class ProjectsController < ApplicationController
     respond_to do |format|
       if @project.update_attributes(params[:project])
         format.html { redirect_to(@project, :notice => 'Project was successfully updated.') }
-        format.xml  { head :ok }
+        format.json  { head :no_content }
       else
         format.html { render :action => "edit" }
-        format.xml  { render :xml => @project.errors, :status => :unprocessable_entity }
+        format.json  { render :json => @project.errors, :status => :unprocessable_entity }
       end
     end
   end
@@ -77,7 +77,7 @@ class ProjectsController < ApplicationController
 
     respond_to do |format|
       format.html { redirect_to(projects_url) }
-      format.xml  { head :ok }
+      format.json  { head :no_content }
     end
   end
 end
